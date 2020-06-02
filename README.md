cd02ff269b8c6255d26e557ede664f27e075ea39
loicgeek <loic.ngou98@gmail.com>
2020-06-02 10:17:05 +0100

    fixed array as accepted function and updated readme

--- README.md
+++ README.md
@@ -1,20 +1,20 @@
-# flut-lang README
+# json-completer
 
-This is the README for your extension "flut-lang". After writing up a brief description, we recommend including the following sections.
+Json-completer is a vscode extension that allows developper to autocomplete nested object from a json file.
 
 ## Features
 
 Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
 
-For example if there is an image subfolder under your extension project workspace:
-
 \!\[feature X\]\(images/feature-x.png\)
-
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
 
 ## Requirements
 
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
+> You should configure the .jcompleter.config file at the root of your project. This file must give the path to you model json file
+
+`{ "model_path":"locales/locale_en.json", "accepted_functions":["AllTranslation.text","translate"] //optional }`
+
+> `accepted_functions` parameter let user choose which functions he want this plugin to suggest json completion for.
 
 ## Extension Settings
 
@@ -24,8 +24,8 @@ For example:
 
 This extension contributes the following settings:
 
-* `myExtension.enable`: enable/disable this extension
-* `myExtension.thing`: set to `blah` to do something
+- `myExtension.enable`: enable/disable this extension
+- `myExtension.thing`: set to `blah` to do something
 
 ## Known Issues
 
@@ -47,19 +47,19 @@ Fixed issue #.
 
 Added features X, Y, and Z.
 
------------------------------------------------------------------------------------------------------------
+---
 
 ## Working with Markdown
 
-**Note:** You can author your README using Visual Studio Code.  Here are some useful editor keyboard shortcuts:
+**Note:** You can author your README using Visual Studio Code. Here are some useful editor keyboard shortcuts:
 
-* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux)
-* Toggle preview (`Shift+CMD+V` on macOS or `Shift+Ctrl+V` on Windows and Linux)
-* Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (macOS) to see a list of Markdown snippets
+- Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux)
+- Toggle preview (`Shift+CMD+V` on macOS or `Shift+Ctrl+V` on Windows and Linux)
+- Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (macOS) to see a list of Markdown snippets
 
 ### For more information
 
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
+- [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
+- [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
 
 **Enjoy!**
--- extension.js
+++ extension.js
@@ -4,6 +4,13 @@ const vscode = require("vscode");
 const fs = require("fs");
 const path = require("path");
 
+const FOLDER_PATH = vscode.workspace.rootPath;
+const CONFIG_FILE_NAME = ".jcompleter.config";
+const CONFIG_PATH = path.join(FOLDER_PATH, CONFIG_FILE_NAME);
+let CONFIG;
+let MODEL_PATH;
+let accepted_functions_STRING;
+let KEYS_LIST;
 const getObjectKeys = (obj, prefix = "") => {
   return Object.entries(obj).reduce((collector, [key, val]) => {
     const newKeys = [...collector, prefix ? `${prefix}.${key}` : key];
@@ -18,41 +25,51 @@ const getObjectKeys = (obj, prefix = "") => {
   }, []);
 };
 
-const FOLDER_PATH = vscode.workspace.rootPath;
-const CONFIG_FILE_NAME = ".jcompleter.config";
-const CONFIG_PATH = path.join(FOLDER_PATH, CONFIG_FILE_NAME);
-if (!fs.existsSync(CONFIG_PATH)) {
-  vscode.window.showErrorMessage(
-    "config file " + CONFIG_FILE_NAME + " not found"
-  );
-}
-let rawdata = fs.readFileSync(CONFIG_PATH, {
-  encoding: "utf-8",
-});
-const CONFIG = JSON.parse(rawdata);
-if (CONFIG["model_path"] == null) {
-  vscode.window.showErrorMessage(
-    "field 'model_path' not found in" + CONFIG_FILE_NAME
-  );
-}
-const MODEL_PATH = path.join(FOLDER_PATH, CONFIG["model_path"]);
+const loadConfig = () => {
+  if (!fs.existsSync(CONFIG_PATH)) {
+    vscode.window.showErrorMessage(
+      "config file " + CONFIG_FILE_NAME + " not found"
+    );
+  }
+  let rawdata = fs.readFileSync(CONFIG_PATH, {
+    encoding: "utf-8",
+  });
+  try {
+    CONFIG = JSON.parse(rawdata);
+  } catch (e) {
+    vscode.window.showErrorMessage(e.toString());
+    return;
+  }
 
-var translateFunctions;
-if (
-  CONFIG["translate_functions"] != null &&
-  CONFIG["translate_functions"].length > 1
-) {
-  translateFunctions = CONFIG["translate_functions"];
-} else {
-  translateFunctions = ["AllTranslations.text"];
-}
+  if (CONFIG["model_path"] == null) {
+    vscode.window.showErrorMessage(
+      "field 'model_path' not found in" + CONFIG_FILE_NAME
+    );
+  } else {
+    console.log("config loaded");
+    return CONFIG;
+  }
+};
 
-const TRANSLATE_FUNCTIONS_STRING = translateFunctions.join("|");
-let jsonData = fs.readFileSync(MODEL_PATH, {
-  encoding: "utf-8",
-});
-let data = JSON.parse(jsonData);
-var list = getObjectKeys(data);
+const loadData = () => {
+  MODEL_PATH = path.join(FOLDER_PATH, CONFIG["model_path"]);
+  var translateFunctions;
+  if (
+    CONFIG["accepted_functions"] != null &&
+    CONFIG["accepted_functions"].length > 1
+  ) {
+    translateFunctions = CONFIG["accepted_functions"];
+  } else {
+    translateFunctions = ["AllTranslations.text"];
+  }
+  accepted_functions_STRING = "(" + translateFunctions.join("|") + ")";
+  let jsonData = fs.readFileSync(MODEL_PATH, {
+    encoding: "utf-8",
+  });
+  KEYS_LIST = getObjectKeys(JSON.parse(jsonData));
+  console.log("data loaded");
+  return KEYS_LIST;
+};
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -61,9 +78,13 @@ var list = getObjectKeys(data);
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+  console.log("json-completer successfully activated");
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
-  let flutterLangProvider = vscode.languages.registerCompletionItemProvider(
+
+  loadConfig();
+  loadData();
+  let jsonCompleterProvider = vscode.languages.registerCompletionItemProvider(
     {
       pattern: "**",
     },
@@ -71,27 +92,27 @@ function activate(context) {
       async provideCompletionItems(document, position) {
         var line = document.lineAt(position);
         const linePrefix = line.text.substr(0, position.character);
-        var regex = TRANSLATE_FUNCTIONS_STRING + '\\("(.*)"\\)';
+        var regex = accepted_functions_STRING + '\\("(.*)"\\)';
 
         //Converting single quotes to double quotes
         var re = new RegExp("'", "g");
         var match = RegExp(regex).exec(line.text.replace(re, '"'));
 
-        if (match === null || match.length === 1 || match[1] === null) {
+        if (match === null || match.length < 3) {
           return undefined;
         }
+        var matchedText = match[2];
 
         try {
-          var filteredList = list.filter((x) => {
-            return x.startsWith(match[1]);
+          var filteredList = KEYS_LIST.filter((x) => {
+            return x.startsWith(matchedText);
           });
-          console.log(filteredList);
           var result = filteredList.map((item) => {
             const snippetCompletion = new vscode.CompletionItem(item);
             //make sure to move the cursor at the right position
             let startPosition = new vscode.Position(
               line.lineNumber,
-              position.character - match[1].length
+              position.character - matchedText.length
             );
             snippetCompletion.range = new vscode.Range(startPosition, position);
             return snippetCompletion;
@@ -105,6 +126,7 @@ function activate(context) {
     ".",
     '"',
     "'"
+
     // triggered whenever a '.' is being typed
   );
 
@@ -116,14 +138,19 @@ function activate(context) {
       async provideCompletionItems(document, position) {
         var line = document.lineAt(position);
         const linePrefix = line.text.substr(0, position.character);
-
+        console.log("configuration changed");
+        loadConfig();
+        loadData();
         return undefined;
       },
     },
-    "."
+    ":",
+    "'",
+    '"',
+    ","
   );
 
-  context.subscriptions.push(flutterLangProvider);
+  context.subscriptions.push(jsonCompleterProvider, configChangedProvider);
 }
 exports.activate = activate;
 
